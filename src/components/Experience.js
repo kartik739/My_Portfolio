ee9dd24b8189972b5007613fa2bfe4d0c72107eb
Kartik Goel <75537543+kartik739@users.noreply.github.com>
2023-06-02 18:49:50 +0530

    Update Experience.js

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -39,7 +39,7 @@ const Experience = () => {
         <h2 className='font-bold text-8xl mb-32 w-full text-center  md:text-6xl xs:text-4xl md:mb-16'>
         Experience
         </h2>
-        <div ref={ref} className='w-[75%] mx-auto lg:w-[90%] md:w-full '>
+        <div ref={ref} className='w-[75%] mx-auto relative lg:w-[90%] md:w-full '>
      
             <motion.div  style={{scaleY: scrollYProgress}}
             className='absolute left-9 top=0 w-[4px] h-full bg-dark origin-top dark:bg-light md:w-[2px] md:left-[30px] xs:left-[20px]'/>
