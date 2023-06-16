b0a066bb6969aa7400c671da47708472da03dbd2
Kartik Goel <75537543+kartik739@users.noreply.github.com>
2023-06-16 18:47:20 +0530

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -78,11 +78,11 @@ const about = () => {
         <div className='col-span-2 flex flex-col items-end justify-between xl:col-span-8 xl:flex-row xl:items-center md:order-3'>
             <div className='flex flex-col items-end justified-center xl:items-center'>
                 <span className='inline-block text-7xl font-bold md:text-6xl sm:text-5xl xs:text-4xl'>
-                    <AnimatedNumber value={50}/>+
+                    <AnimatedNumber value={15}/>+
                  </span>
                  <h2 className='text-xl font-medium capitalize text-dark/75 dark:text-light/75 xl:text-center
                   md:text-lg sm:text-base xs:text-sm'> 
-                    Satisfied Customers
+                    Hackathon Attended
                  </h2>
             </div>
             <div className='flex flex-col items-end justified-center xl:items-center'>
